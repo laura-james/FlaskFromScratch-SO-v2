2b11cebd7c7053e6e65f3f93bd7fa07fe644103e
Mrs James <lollypopstar70@gmail.com>
2022-02-09 20:58:44 +0000

    Added more css

--- main.py
+++ main.py
@@ -41,47 +41,6 @@ def login():
       msg = "incorrect password"
   return render_template("login.html",msg = msg)
 
-@web_site.route('/runonce')
-def createDBandTable():
-  con = sqlite3.connect('movies.db')
-  sql = """
-  CREATE TABLE Movies(id INTEGER,
-  name TEXT,
-  description TEXT,
-  year INTEGER,
-  PRIMARY KEY(id AUTOINCREMENT))
 
-  """
-  cursor = con.cursor()
-  cursor.execute(sql)
-  con.commit()
-  return "database and table created"
-
-@web_site.route('/moviesadd',methods = ['GET', 'POST'])
-def moviesadd():
-  msg = ""
-  if request.method == 'POST':
-    name = request.form["name"]
-    desc = request.form["descr"]
-    year = request.form["year"]
-    con = sqlite3.connect('movies.db')
-    sql = "INSERT INTO Movies(name,description,year) VALUES(?,?,?)"
-    cursor = con.cursor()
-    cursor.execute(sql,(name,desc,year))
-    con.commit()
-    msg = name + " added to the Movies table"
-  return render_template("addmovie.html",msg = msg)
-
-@web_site.route('/movieslist')
-def listall():
-  con = sqlite3.connect('movies.db')
-  con.row_factory = sqlite3.Row
-  cursor = con.cursor()
-  sql = 'SELECT * FROM movies'
-  cursor.execute(sql)
-  con.commit()
-  rows = cursor.fetchall() 
-  return render_template("movielist.html",rows = rows)
-  
 #this is the last line!
 web_site.run(host='0.0.0.0', port=8080)
